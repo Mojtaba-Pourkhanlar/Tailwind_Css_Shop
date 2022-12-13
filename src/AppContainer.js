cd9c36a0068eae7f8e0bd62406af5c77e4f05f90
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2022-12-13 21:43:11 +0330

    custom-config

---
+++ src/AppContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "./frontEnd/components/Header";
+
+const AppContainer = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default AppContainer;
